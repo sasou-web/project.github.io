a247b7df6c17959e5a8f1ff3511056e014b6a479
sasou-web <samir@sasou.fr>
2025-02-09 23:34:45 +0100

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-// assets/js/index.js
-document.addEventListener('DOMContentLoaded', () => {
-  // À chaque clic n’importe où dans le body, on redirige vers presentation.html
-  document.body.addEventListener('click', () => {
-    window.location.href = 'presentation.html';
-  });
-});
